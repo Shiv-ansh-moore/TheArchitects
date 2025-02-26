7dbb084007beaa1baed03a9fce0d63e642b1df5b
Shivansh Moore <shivansh.moore57@mail.dcu.ie>
2025-02-26 07:27:05 +0000

    about fade in working

--- thearchitectsie/app/components/About.js
+++ thearchitectsie/app/components/About.js
@@ -1,14 +1,32 @@
 import Navbar from "./Navbar";
+import styles from "./Fade_in.module.css";
 
-export default function About({handleAboutClick, handleHomeClick}) {
+export default function About({ handleAboutClick, handleHomeClick }) {
   return (
-    <div className="bg-architectsRed min-h-screen w-screen -mt-3 -mr-32 -ml-32">
-      <div className="mr-32 ml-32 pt-3">
+    <div>
+      <div
+        className={`bg-architectsRed min-h-screen w-screen -mt-3 -mr-32 -ml-32 absolute ${styles.fadeIn}`}
+      />
+      <div className="z-10 relative">
         <Navbar
           aColor="#FFFBF5"
           handleAboutClick={handleAboutClick}
           handleHomeClick={handleHomeClick}
         />
+        <p className="text-background max-w-96 my-16 text-xl">
+          We are an architectural practice located in Dun Laoghaire, Co. Dublin.
+          Our team of architects have extensive experience in domestic and
+          commercial work, from home renovations and extensions through to
+          one-off houses, housing developments and commercial fit outs. At a
+          larger scale we collaborate with other field specialists to bring
+          projects through to fruition.
+        </p>
+        <button
+          className="mr-24 bg-background w-24 h-8 rounded-3xl font-bold text-xl"
+          onClick={handleHomeClick}
+        >
+          Back
+        </button>
       </div>
     </div>
   );
